860acf4800d8e1a88da0b67390a305adfb9a033d
Gaurav-k-Gupta <gauravgupta0954@gmail.com>
2024-11-07 19:01:01 +0700

    Updated navbar

--- src/App.js
+++ src/App.js
@@ -3,28 +3,28 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function BasicExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Container className="bg-color">
+        <Navbar.Brand href="#home">Hostel-Management</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto  justify-center">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="#link">Hostels</Nav.Link>
+            <NavDropdown title="Academics" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Library</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Engineering Departments
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Lecture Hall Complex</NavDropdown.Item>
+              
             </NavDropdown>
+            <Nav.Link href="#about">About Us</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
